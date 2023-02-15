fa49b7a4fce22bdbeb868dad7ae64a3c84aaf8e4
Vicky Rana <vickyisrulin@users.noreply.github.com>
2023-02-15 11:29:06 -0800

    HELLO
    Testing123!

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = ""
+    var title:   String = "HELLO"
     var content: String = ""
     
     var id: String = UUID().uuidString
